82d301066601fe5162c0f3776320b4957ea5c9b7
Taylor <taylorfulner@gmail.com>
2021-05-25 15:23:57 -0400

    Add Rating Model

---
+++ RestaurantRaterAPI/Models/Rating.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Rating
+    { 
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Restaurant))]
+        public int RestaurantId { get; set; }
+        public virtual Restaurant Restaurant { get; set; }
+
+        [Required]
+        [Range(0,10)]
+        public double FoodScore { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public double EnvironmentScore { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public double CleanlinessScore { get; set; }
+        public double AverageRating => (FoodScore + EnvironmentScore + CleanlinessScore) / 3;
+    }
+}
