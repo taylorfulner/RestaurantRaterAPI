b11d9b453d7c76ed7a1df0bec96de6470fdd957f
Taylor <taylorfulner@gmail.com>
2021-05-25 15:02:25 -0400

    Added UpdateRestaurant and DeleteRestaurant controller

--- RestaurantRaterAPI/Controllers/RestaurantController.cs
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -39,6 +39,7 @@ namespace RestaurantRaterAPI.Controllers
             List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
             return Ok(restaurants);
         }
+
         //GetById
         [HttpGet]
         public async Task<IHttpActionResult> GetById(int id)
@@ -52,5 +53,46 @@ namespace RestaurantRaterAPI.Controllers
             return NotFound();
         }
         //Update(PUT)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant(int id, Restaurant updatedRestaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+                if(restaurant != null)
+                {
+                    restaurant.Name = updatedRestaurant.Name;
+                    restaurant.Address = updatedRestaurant.Address;
+                    restaurant.Rating = updatedRestaurant.Rating;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+        }
+
+        //Delete
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurant(int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if(restaurant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Restaurants.Remove(restaurant);
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("The restaurant was successfully deleted.");
+            }
+
+            return InternalServerError();
+        }
     }
 }
