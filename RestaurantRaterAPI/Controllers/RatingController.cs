e337d4dcae9e7e9653e7ff1f492a08c4b7eb7c6b
Taylor <taylorfulner@gmail.com>
2021-05-25 16:46:27 -0400

    Added Rating Controller

---
+++ RestaurantRaterAPI/Controllers/RatingController.cs
@@ -0,0 +1,39 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RatingController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        [HttpPost]
+        public async Task<IHttpActionResult> PostRating(Rating model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Restaurant restaurant = await _context.Restaurants.FindAsync(model.RestaurantId);
+            if (restaurant == null)
+            {
+                return BadRequest($"The target restaurant with the Id of {model.RestaurantId} does not exist.");
+            }
+
+            _context.Ratings.Add(model);
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok($"You rated {restaurant.Name} successfully.");
+            }
+
+            return InternalServerError();
+        }
+    }
+}
